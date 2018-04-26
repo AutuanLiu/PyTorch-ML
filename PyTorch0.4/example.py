1544156c8daceba4e27d35053a24f6a1205fffff
AutuanLiu <autuanliu@163.com>
2018-04-26 10:14:36 +0800

    fix errors

--- PyTorch0.4/example.py
+++ PyTorch0.4/example.py
@@ -17,21 +17,37 @@ def get_data(flag=True):
 
 # 网络模型定义
 class Network(Module):
+    # def __init__(self):
+    #     super().__init__()
+    #     self.l1 = torch.nn.Linear(config['in_feature'], 500)
+    #     self.l2 = torch.nn.Linear(500, 350)
+    #     self.l3 = torch.nn.Linear(350, 200)
+    #     self.l4 = torch.nn.Linear(200, 130)
+    #     self.l5 = torch.nn.Linear(130, config['out_feature'])
+
+    # def forward(self, x):
+    #     data = x.view(-1, config['in_feature'])
+    #     y = F.relu(self.l1(data))
+    #     y = F.relu(self.l2(y))
+    #     y = F.relu(self.l3(y))
+    #     y = F.relu(self.l4(y))
+    #     return self.l5(y)
     def __init__(self):
         super().__init__()
-        self.l1 = torch.nn.Linear(config['in_feature'], 500)
-        self.l2 = torch.nn.Linear(500, 350)
-        self.l3 = torch.nn.Linear(350, 200)
-        self.l4 = torch.nn.Linear(200, 130)
-        self.l5 = torch.nn.Linear(130, config['out_feature'])
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = torch.nn.Dropout2d()
+        self.fc1 = torch.nn.Linear(320, 50)
+        self.fc2 = torch.nn.Linear(50, 10)
 
     def forward(self, x):
-        data = x.view(-1, config['in_feature'])
-        y = F.relu(self.l1(data))
-        y = F.relu(self.l2(y))
-        y = F.relu(self.l3(y))
-        y = F.relu(self.l4(y))
-        return self.l5(y)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
 
 def train_m(mod, data_loader):
@@ -47,13 +63,13 @@ def train_m(mod, data_loader):
             len1 = batch_idx * len(data)
             len2 = len(data_loader.dataset)
             pec = 100. * batch_idx / len(data_loader)
-            print(f"Train Epoch: {epoch + 1} [{len1:5d}/{len2:5d} ({pec:3.2f}%)] \t Loss: {loss.detach():.5f}")
+            print(f"Train Epoch: {epoch + 1} [{len1:5d}/{len2:5d} ({pec:3.2f}%)] \t Loss: {loss.item():.5f}")
 
 
 def test_m(mod, data_loader):
+    mod.eval()
+    test_loss, correct = 0, 0
     with torch.no_grad():
-        mod.eval()
-        test_loss, correct = 0, 0
         for data, target in data_loader:
             data, target = data.to(device), target.to(device)
             output = mod(data)
@@ -61,7 +77,7 @@ def test_m(mod, data_loader):
             test_loss += criterion(output, target).item()
             # get the index of the max
             _, pred = output.data.max(1, keepdim=True)
-            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(data_loader.dataset)
     len1 = len(data_loader.dataset)
@@ -75,7 +91,7 @@ if __name__ == '__main__':
     train_loader, test_loader = get_data(), get_data(flag=False)
     # 模型实例与损失函数, 优化函数
     model = Network().to(device)
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.NLLLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=config['lr'], momentum=0.9)
     # 训练与测试
     for epoch in range(config['epoch_num']):
