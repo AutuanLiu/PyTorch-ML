ca8aeee8754975069ee2067b0fc6ae404f2f6197
e <autuanliu@163.com>
2018-09-20 20:10:52 +0800

    add new model

--- SimpleCNN/cifar10cnn.py
+++ SimpleCNN/cifar10cnn.py
@@ -6,12 +6,15 @@ Date: 2018/9/16
 import os
 
 import torch
+import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
 
+# device 设置
+dev = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # 定义网络
 class CifarCNN(nn.Module):
@@ -20,9 +23,9 @@ class CifarCNN(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool1 = nn.MaxPool2d(2, stride=2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(256 , 120)
-        self.fc2 = nn.Linear(120, 70)
-        self.fc3 = nn.Linear(70, 10)
+        self.fc1 = nn.Linear(400 , 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         out = self.pool1(F.relu(self.conv1(x)))
@@ -34,35 +37,34 @@ class CifarCNN(nn.Module):
 
 
 # 获取数据
-def get_data(root='datasets/cifar10/', flag=True, bs=64, num_work=4, trans=transforms.ToTensor()):
+def get_data(root='datasets/cifar10/', flag=True, bs=64, tsfm=transforms.ToTensor()):
     if not os.path.exists(root):
         os.makedirs(root)
         flag1 = True
     else:
         flag1 = False
-    mnist = CIFAR10(root, train=flag, transform=trans, download=flag1)
-    loader = DataLoader(mnist, batch_size=bs, shuffle=flag, drop_last=False, num_workers=num_work)
+    data = CIFAR10(root, train=flag, transform=tsfm, download=flag1)
+    loader = DataLoader(data, batch_size=bs, shuffle=flag, drop_last=False)
     return loader
 
 
-trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+tfs = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-train_loader = get_data(bs=32)
-test_loader = get_data(flag=False, bs=32)
-
-# device 设置
-dev = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+train_loader = get_data(bs=32, tsfm=tfs)
+test_loader = get_data(flag=False, bs=32, tsfm=tfs)
 
 # 网络配置
+n_class = 10
 net = CifarCNN().to(dev)
 print(net)
 criterion = nn.CrossEntropyLoss()
-opt = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+opt = optim.Adam(net.parameters(), lr=1e-4)
 num_epoch = 100
 
 # 训练网络
+loss_his = torch.zeros(num_epoch)
 for epoch in range(num_epoch):
-    for idx, (img, label) in enumerate(train_loader):
+    for _, (img, label) in enumerate(train_loader):
         img, label = img.to(dev), label.to(dev)
 
         # 前向传播
@@ -74,20 +76,32 @@ for epoch in range(num_epoch):
         loss.backward()
         opt.step()
 
-    # 输出训练结果
+    # 记录并输出训练结果
+    loss_his[epoch] = loss.item()
     print(f'Epoch [{epoch+1}/{num_epoch}], Loss [{loss.item():.4f}]')
 
 # 评估网络(此时不需要计算grad)
 net.eval()
 with torch.no_grad():
-    correct, total = 0, 0
+    correct = total = torch.zeros(n_class).to(dev)
     for img, label in test_loader:
         img, label = img.to(dev), label.to(dev)
         out = net(img)
         _, pred = torch.max(out, 1)
-        total += label.shape[0]
-        correct += (pred == label).sum().item()
-print(f'Test acc on the 10000 test img: {100 * correct / total}%')
+        len1 = label.shape[0] # 数据之前会隐藏一个维度 batchsize
+        is_correct = (pred == label)
+        # 对每个数据对判断
+        for idx in range(len1):
+            lbs_true = label[idx].item()
+            total[lbs_true] += 1
+            correct[lbs_true] += is_correct[idx].item()
+for idx in range(n_class):
+    print(f'Test acc of class {idx+1} is: { 100 * correct[idx] / total[idx].item():.4f}%')
 
 # 保存 checkpoint
 torch.save(net.state_dict(), 'SimpleCNN/cifarnet.ckpt')
+
+# 可视化 loss
+plt.plot(loss_his.numpy())
+plt.xlabel('epoch'); plt.ylabel('loss'); plt.title('epoch vs loss')
+plt.show()
--- SimpleCNN/simplecnn.py
+++ SimpleCNN/simplecnn.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.datasets import FashionMNIST
+from torchvision.datasets import MNIST
 
 # device 设置
 dev = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -30,29 +30,28 @@ class SimpleCNN(nn.Module):
         return self.fc(out)
 
 
-# 实例化网络
-net = SimpleCNN().to(dev)
-print(net)
-
-# 网络配置
-criterion = nn.CrossEntropyLoss()
-opt = optim.Adam(net.parameters(), lr=0.1)
-num_epoch = 10
-n_class = 1
-
 # 获取数据
-def get_data(root='datasets/fashionmnist/', flag=True, bs=64, trans=transforms.ToTensor()):
+def get_data(root='datasets/mnist/', flag=True, bs=64, trans=transforms.ToTensor()):
     if not os.path.exists(root):
         os.makedirs(root)
         flag1 = True
     else:
         flag1 = False
-    mnist = FashionMNIST(root, train=flag, transform=trans, download=flag1)
+    mnist = MNIST(root, train=flag, transform=trans, download=flag1)
     loader = DataLoader(mnist, batch_size=bs, shuffle=flag, drop_last=False)
     return loader
+    
+# 实例化网络
+net = SimpleCNN().to(dev)
+print(net)
 
+# 网络配置
+n_class      = 10
 train_loader = get_data()
-test_loader = get_data(flag=False)
+test_loader  = get_data(flag=False)
+criterion    = nn.CrossEntropyLoss()
+opt          = optim.Adam(net.parameters(), lr=0.01)
+num_epoch    = 100
 
 # 训练网络
 for epoch in range(num_epoch):
@@ -61,6 +60,7 @@ for epoch in range(num_epoch):
 
         # 前向传播
         out = net.forward(img)    # or out = net(img)
+        # The losses are averaged across observations for each minibatch
         loss = criterion.forward(out, label)    # or loss = criterion(out, label)
 
         # 后向传播
@@ -68,7 +68,7 @@ for epoch in range(num_epoch):
         loss.backward()
         opt.step()
 
-    # 输出训练结果
+    # 每个 epoch 输出训练结果
     print(f'Epoch [{epoch+1}/{num_epoch}], Loss [{loss.item():.4f}]')
 
 # 评估网络(此时不需要计算grad)
@@ -83,22 +83,21 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (pred == labels).sum().item()
 
-print(f'Accuracy of the network on the 10000 test images: {100 * correct / total}%')
+print(f'Accuracy of the network on the 10000 test images: {100 * correct / total:.4f}%')
 
 # 在各个类别上的表现
 with torch.no_grad():
-    correct = list(0. for i in range(n_class))
-    total = list(0. for i in range(n_class))
+    correct = [0.] * n_class
+    total = [0.] * n_class
     for img, label in test_loader:
         img, label = img.to(dev), label.to(dev)
         out = net(img)
         _, pred = torch.max(out, 1)
-        cor = (pred == label).squeeze()
+        cor = (pred == label).cpu()
         len1 = label.shape[0]
-        print(len1)
         for idx in range(len1):
             # 哈希表
-            lbs = label[idx]
+            lbs = label[idx].item()
             correct[lbs] += cor[idx].item()
             total[lbs] += 1
 
